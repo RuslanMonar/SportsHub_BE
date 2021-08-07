7aae24c6b94dc02e5b9d5921bc8321ca58d99354
qwerhnatkiv <qwerhnatkiv@gmail.com>
2021-08-07 04:01:27 +0300

    Connect Db and project

--- API/Startup.cs
+++ API/Startup.cs
@@ -2,15 +2,19 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+
 
 namespace API
 {
@@ -28,6 +32,10 @@ namespace API
         {
 
             services.AddControllers();
+            services.AddDbContext<DataContext>(opt => opt.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
---
+++ Data/Seed.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    public class Seed
+    {
+        public static async Task SeedData(DataContext context, UserManager<IdentityUser> userManager)
+        {
+            if (!userManager.Users.Any())
+            {
+                var users = new List<IdentityUser>
+                {
+                    new IdentityUser{UserName = "bob", Email = "bob@test.com"},
+                    new IdentityUser{UserName = "tom", Email = "tom@test.com"},
+                    new IdentityUser{UserName = "jane", Email = "jane@test.com"},
+                };
+
+                foreach (var user in users)
+                {
+                    await userManager.CreateAsync(user, "PassworD1!");
+                }
+            }
+
+        }
+    }
+}
