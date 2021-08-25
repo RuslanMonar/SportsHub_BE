883b929914f22fe0ab30b6487889ab80aca95a41
RuslanMonar <80795967+RuslanMonar@users.noreply.github.com>
2021-08-25 17:17:16 +0300

    Update ChangePasswordResult.cs

--- Application/ChangePasswordResult.cs
+++ Application/ChangePasswordResult.cs
@@ -5,8 +5,6 @@ namespace Application
 {
     public class ChangePasswordResult: IResult
     {
-        public bool Success { get; set; }
-
         public IEnumerable<string> Errors { get; set; }
 
         public bool Success { get; set; }
