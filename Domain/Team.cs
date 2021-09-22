40001a4a099683b62c92991eb43846c555ebf0be
qwerhnatkiv <qwerhnatkiv@gmail.com>
2021-09-22 18:34:19 +0300

    Create Team Model

---
+++ Domain/Team.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Domain
+{
+    public class Team
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Location { get; set; }
+
+        public string Category { get; set; }
+
+        public string SubCategory { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public DateTime AddedAt { get; set; }
+    }
+}
